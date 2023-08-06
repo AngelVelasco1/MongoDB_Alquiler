072329c6928ec6a478808d05f95f32ad251a14d0
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-08-06 13:20:57 -0500

    feat: :wrench: env config and main file basic config

---
+++ app.js
@@ -0,0 +1,21 @@
+//? Dependencies 
+import express from 'express';
+import dotenv from 'dotenv';
+import storageAutomovil from './routes/automovil.js';
+
+const app = express();
+app.use(express.json()); 
+app.use(express.text());
+
+//? Routes
+app.use('/utomovil', storageAutomovil);
+
+//? Env
+dotenv.config();
+
+//? Server
+const server = JSON.parse(process.env.SERVER);
+app.listen(server, () => {
+    console.log(`http://${server.hostname}:${server.port}`);
+})
+
