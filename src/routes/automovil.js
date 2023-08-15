a8592357e151a992ffafe50270d1d11766b09d9e
Angel Velasco <angelgg2020@outlook.com>
2023-08-15 08:39:12 -0500

    feat: :sparkles: Delete automoviles

--- src/routes/automovil.js
+++ src/routes/automovil.js
@@ -9,32 +9,59 @@ const storageAutomovil = Router();
 
 //? General Variables
 let db = await conx();
-let automovil = 
- db.collection("automovil");
+let automovil = db.collection("automovil");
 
 //? Get automoviles
 storageAutomovil.get("/", limitGrt(), proxyAutomovil, async (req, res) => {
   if (!req.rateLimit) return;
-
-  let automoviles = await automovil.find({}).toArray();
-  res.send(automoviles);
+  try {
+    let automoviles = await automovil.find({}).toArray();
+    res.send(automoviles);
+  } catch (err) {
+    res.status(422).send({ Error: err.message });
+  }
 });
 
 //? Add automoviles
-storageAutomovil.post("/add", limitGrt(), proxyAutomovil, dtoData, async (req, res) => {
-  if (!req.rateLimit) return;
-  try {
-    let newAutomovil = await automovil.insertOne(req.body);
-    res.status(201).send({Added: newAutomovil})
-
-  } catch(err) {
-      res.status(422).send({Error: err.message})
+storageAutomovil.post(
+  "/add",
+  limitGrt(),
+  proxyAutomovil,
+  dtoData,
+  async (req, res) => {
+    if (!req.rateLimit) return;
+    try {
+      let newAutomovil = await automovil.insertOne(req.body);
+      res.status(201).send({ Added: newAutomovil });
+    } catch (err) {
+      res.status(422).send({ Error: err.message });
+    }
   }
-
-});
+);
 
 //? Delete automoviles
+storageAutomovil.delete("/remove/:id?",
+  limitGrt(),
+  proxyAutomovil,
+  async (req, res) => {
+    if (!req.rateLimit) return;
+
+    try {
+      if(!parseInt(req.params.id)) {
+        res.status(404).send({Error: "Not Automovil found"}) ;
+      } else {
+        let delAutomovil = await automovil.deleteOne({"ID_automovil": parseInt(req.params.id)});
+        res.status(200).send({ Deleted: delAutomovil})
+      }
+    
+    } catch (err) {
+      res.status(422).send({ Error: err.message });
+    }
+  }
+);
 
 //? Update automoviles
+storageAutomovil.patch("/update/:_id?", limitGrt(), proxyAutomovil, (req, res) => {
 
+})
 export default storageAutomovil;
