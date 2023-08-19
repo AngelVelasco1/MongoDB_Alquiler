b28c99af0596ba375db18d422fbf43389d300499
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-08-19 10:34:55 -0500

    feat: :sparkles: Obtener el costo total de un alquiler especifico

--- src/routes/alquiler.js
+++ src/routes/alquiler.js
@@ -11,13 +11,39 @@ let db = await conx();
 let alquiler = db.collection("alquiler");
 
 //?  Obtener los detalles del alquiler con el ID_Alquiler específico. 
-storageAlquiler.get('/:ID_alquiler', limitGrt(), classVerify, async(req, res) => {
+storageAlquiler.get('/:ID_alquiler', limitGrt(), classVerify, async (req, res) => {
     const alquilerID = parseInt(req.params.ID_alquiler);
     const specificAlquiler = await alquiler.findOne(
-        { "ID_alquiler": alquilerID  }
-        );
+        { "ID_alquiler": alquilerID }
+    );
 
-    res.status(200).send({Alquiler: specificAlquiler})
-} )
+    res.status(200).send({ Alquiler: specificAlquiler })
+});
+
+//? 8. Obtener el costo total de un alquiler específico. 
+storageAlquiler.get('/total/:ID_alquiler', limitGrt(), classVerify, async (req, res) => {
+    const alquilerID = parseInt(req.params.ID_alquiler);
+    const totalAlquiler = await alquiler.aggregate([
+        {
+            $match: {
+                ID_alquiler: { $eq: alquilerID }
+            }
+        },
+        {
+            $project: {
+                "_id": 0,
+                "ID_alquiler": 0,
+                "ID_Automovil_id": 0,
+                "ID_Cliente_id": 0,
+                "Fecha_Inicio": 0,
+                "Fecha_Fin": 0,
+                "Fecha_Fin": 0,
+                "Estado": 0
+            }
+        }
+    ]).toArray();
+
+    res.status(200).send(totalAlquiler)
+});
 
 export default storageAlquiler;
